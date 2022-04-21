d1c25741f59c7097f5cfca6fbf9831570eaa170d
pkgajera <pkgajera007@gmail.com>
2022-04-21 17:56:19 +0530

    fixed products test error

--- src/__tests__/Products.test.tsx
+++ src/__tests__/Products.test.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import '../setupTest'
-import Products from '../components/Products'
+import React from "react";
+import "../setupTest";
+import Products from "../components/Products";
+import { BrowserRouter } from "react-router-dom";
+import { mount } from "enzyme";
 
-describe('Products test', () => {
-  it('should render the component products', () => {
-    const wrapper = shallow(<Products />)
-    expect(wrapper).not.toBe(null)
-  })
-})
+let wrapper: any;
+describe("Products test", () => {
+  beforeEach(() => {
+    wrapper = mount(
+      <BrowserRouter>
+        <Products />
+      </BrowserRouter>
+    );
+  });
+
+  it("should render the component products", () => {
+    // const wrapper = shallow(<Products />)
+    expect(wrapper).not.toBe(null);
+  });
+});
